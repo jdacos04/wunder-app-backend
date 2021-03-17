09581bdf53b9e9a70c0bed7034b543de949d180d
jdacos04 <57870511+jdacos04@users.noreply.github.com>
2021-03-16 23:30:01 -0400

    el xD vergacion

--- src/database.js
+++ src/database.js
@@ -6,7 +6,8 @@ const pool =new postgres.Pool({
     host: 'ec2-3-95-85-91.compute-1.amazonaws.com',
     password: 'e7f64f04345c22796d857a79cc6a17d66055e89bdce80df6d52128cebcd9d461',
     database: 'd2sbrm1clg819l',
-    port: '5432'
+    port: '5432',
+    ssl: true
 });
 
 // const pool =new postgres.Pool({
